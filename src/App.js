6dea83395ef0d068cf753fb3f7f1d839bdd8cf77
mubarizmayilzade <mubarizbm@code.edu.az>
2022-06-27 02:41:50 +0400

    Navbar component is done.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import GlobalStyle from './globalStyles';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar } from './components';
-
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <GlobalStyle/>
       <Navbar/>
-    </div>
+    </Router>
   );
 }
 
